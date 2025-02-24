3ac7c72881c9321b84e35fbd92acb2f0afce3cfd
Jgmor4es <freitas.analuiza29@gmail.com>
2025-02-24 11:22:35 -0400

    add exercicios estrutura de dados

---
+++ estrutura-dados/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// estrutura de dados vetor array
+int[] meuArray = {1,2,3,4,5};
+
+//foreach para percorrer o vetor
+
+foreach (int numero in meuArray)
+{
+    Console.WriteLine(numero);
+}
+
+//estrutura de dados lista
+List<string> listaNomes = new List<string>{"Wanderson", "Jeane", "Ryan"};
+listaNomes.Add("Maria");
+
+foreach (string nome in listaNomes)
+{
+Console.WriteLine(nome);
+}
+
+//remover da lista
+
+listaNomes.Remove("Jeane");
+
+foreach (string nome in listaNomes)
+{
+    Console.WriteLine(nome);
+}
+//Conjunto (ste)
+
+HashSet<int> conjunto = new HashSet<int>{1, 2,3};
+conjunto.Add(4);
+
+foreach (int numero in conjunto)
+{
+    Console.WriteLine(numero);
+}
+Console.WriteLine("");
+//adicionar um elemento que já existe
+
+conjunto.Add(2);
+
+foreach (int numero in conjunto)
+{
+    Console.WriteLine(numero);
+}
---
+++ exercicio-estrutura-dados/Program.cs
@@ -0,0 +1,34 @@
+﻿//crie um array e somw todos os elementos e exiba o resultado
+
+int[] array = { 1, 2, 3, 4, 5 };
+int total = 0;
+
+foreach (int num in array)
+{
+    total += num;
+}
+Console.WriteLine(total);
+
+// dada uma lista de string list
+ List<string> nomes = new List<string> { "Jeane", "Wanderson", "Jeane", "Ryan", "Jeane" };
+        int contagem = nomes.Count(nome => nome == "Jeane");
+        Console.WriteLine();
+        
+ // Exercício 3: Remover elementos duplicados da lista de inteiro
+   List<int> numeros = new List<int> { 1, 2, 3, 4, 5, 2, 3, 4, 5 };
+   numeros = numeros.Distinct().ToList();
+   Console.WriteLine("Números sem duplicados: " + string.Join(", ", numeros));
+
+  // Exercício 4: Verificar se "Banana" está no conjunto HashSet
+  HashSet<string> frutas = new HashSet<string> { "Maçã", "Laranja", "Banana", "Uva", "Melão" };
+  bool existeBanana = frutas.Contains("Banana");
+  Console.WriteLine("A fruta 'Banana' existe no HashSet: " + existeBanana);
+
+
+ // Exercício 5: Criar uma lista vazia e adicionar números de 1 a 5
+ List<int> numerosNovos = new List<int>();
+ for (int i = 1; i <= 5; i++)
+ {
+     numerosNovos.Add(i);
+ }
+ Console.WriteLine("Números adicionados à lista: " + string.Join(", ", numerosNovos));
