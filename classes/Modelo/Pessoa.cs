80eafda939c6890d281ca0e911eeb9f9ef7950ab
Jgmor4es <freitas.analuiza29@gmail.com>
2025-03-10 08:38:53 -0400

    add metodos na classe pessoa

---
+++ classes/Modelo/Pessoa.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace classes.Modelo
+{}
+    public class Pessoa
+    {
+        // atributos
+        string nome;
+        int idade;
+        double altura;
+        double peso;
+
+        //Construtor
+        public Pessoa(string nome, int idade, double altura, double peso)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.altura = altura;
+            this.peso = peso;
+        }
+    // metodo falar e andar
+    public void Andar()
+    {
+        Console.WriteLine($"{nome} está andando.");
+    }
+    public void Falar()
+    {
+    Console.WriteLine($"{nome} Vou ficar rico programando em C#!");
+    }
+}
