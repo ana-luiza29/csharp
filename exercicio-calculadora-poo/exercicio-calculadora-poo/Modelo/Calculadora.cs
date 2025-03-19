cade22eb6c73fc45c117ff5744853f87bdcd0dbd
Jgmor4es <freitas.analuiza29@gmail.com>
2025-03-19 09:35:07 -0400

    add finished exercicio calculadora

---
+++ exercicio-calculadora-poo/exercicio-calculadora-poo/Modelo/Calculadora.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace exercicio_calculadora.Modelo
+{
+    public class Calculadora
+    {
+    public double Somar(double a, double b)
+    {
+        return a + b;
+    }
+
+    public double Subtrair(double a, double b)
+    {
+        return a - b;
+    }
+
+    public double Multiplicar(double a, double b)
+    {
+        return a * b;
+    }
+
+    public double Dividir(double a, double b)
+    {
+        if (b == 0)
+        {
+            Console.WriteLine("Erro: Divisão por zero!");
+            return double.NaN; // Retorna "Not a Number" se tentar dividir por zero
+        }
+        return a / b;
+    }
+}
+
+}
---
+++ exercicio-calculadora-poo/exercicio-calculadora-poo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using exercicio_calculadora.Modelo;
+
+class Program
+{
+    static void Main()
+    {
+        Calculadora calc = new Calculadora(); 
+
+        Console.WriteLine("Escolha uma operação: +, -, *, /");
+        string operacao = Console.ReadLine(); 
+
+        Console.Write("Digite o primeiro número: ");
+        double num1 = Convert.ToDouble(Console.ReadLine()); 
+
+        Console.Write("Digite o segundo número: ");
+        double num2 = Convert.ToDouble(Console.ReadLine()); 
+
+        double resultado = 0;
+
+        
+        switch (operacao)
+        {
+            case "+":
+                resultado = calc.Somar(num1, num2);
+                break;
+            case "-":
+                resultado = calc.Subtrair(num1, num2);
+                break;
+            case "*":
+                resultado = calc.Multiplicar(num1, num2);
+                break;
+            case "/":
+                resultado = calc.Dividir(num1, num2);
+                break;
+            default:
+                Console.WriteLine("Operação inválida!");
+                return; 
+        }
+
+
+        Console.WriteLine($"Resultado: {resultado}");
+    }
+}
